0728d48e1d5c93733e82b3d7a341dcc34b490ee9
Alexis Araujo <ala1289@gmail.com>
2018-02-02 11:35:40 -0600

    Change in files

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,13 @@
 FROM ubuntu:16.04
 
 #Instala git en la instancia
+RUN apt-get update
 RUN apt-get update && apt-get install -y git
 
 #Instala vim
 RUN apt-get install -y vim
+
+RUN apt-get install -y python2.7 python-pip
 
 #Configurar el servicio de ssh
 
@@ -24,18 +27,27 @@ EXPOSE 22
 CMD ["/usr/sbin/sshd", "-D"]
 
 #Clona el repositorio de git
-RUN cd /home;git clone https://github.com/ciberdarc/testssis.git
+#RUN cd /home;git clone https://github.com/ciberdarc/testssis.git
 
 #WORKDIR /valuations
 #RUN pwd
+
+RUN rm -rf /home/testssis
+
+RUN mkdir -p /home/testssis
+RUN chmod 700 /home/testssis
 
 RUN find /home/testssis -type f -exec chmod 644 {} \;
 
 ADD valuations.csv /home/testssis
 ADD FormatFileValuations.sh /home/testssis
+ADD TabDelimiter.py /home/testssis
+
+RUN cd /home/testssis; python TabDelimiter.py valuations.csv > file.csv
 
 #Ejecuta el sh FormatFileValuations.sh y la pasa el paramtro del csv
-#RUN cd /home/testssis; sh ./FormatFileValuations.sh valuations.csv
+RUN cd /home/testssis; sed -i -e 's/\r$//' FormatFileValuations.sh
+RUN cd /home/testssis; sh ./FormatFileValuations.sh file.csv
 
 #Crea los links simbolicos
 #RUN ln -s /home/testssis/FormatFileValuations.sh /FormatFileValuations.sh
--- FormatFileValuations.sh
+++ FormatFileValuations.sh
@@ -11,18 +11,16 @@ echo "$tiempo"
 echo "Inicia conversion de archivo"
 
 #Imprimir los headers que se ocupan en la tabla fact_valuations en la base de datos WH_DEV_Macquarie 
-echo "fact_validation_id,valuation_desc,day_id,validation_building_excel,currency_cd,asset_reciclyn_program,Reversionary_Terminal_Cap_Rate,Capex_US_Sqft_yr,Discount_Rate,Cap_Rate,IRR,Going_In_Cap_Rate,NOI_Budget_USD,NOI_Budget_MXN,NOI_1_year_USD,NOI_1_year_MXN,Leasing_Managers_Rent,DCF_USD,DCF_MXN,YR_1_NOI_STABILIZED_USD,YR_1_NOI_STABILIZED_MXN,Market_Rent_APP,Comparison_Approach_USD,Comparison_Approach_MXN,Final_Val_USD,Final_Val_MXN," > header.txt
+echo "fact_validation_id|valuation_desc|day_desc|validation_building_excel|currency_cd|asset_reciclyn_program|Reversionary_Terminal_Cap_Rate|Capex_US_Sqft_yr|Discount_Rate|Cap_Rate|IRR|Going_In_Cap_Rate|NOI_Budget_USD|NOI_Budget_MXN|NOI_1_year_USD|NOI_1_year_MXN|Leasing_Managers_Rent|DCF_USD|DCF_MXN|YR_1_NOI_STABILIZED_USD|YR_1_NOI_STABILIZED_MXN|Market_Rent_APP|Comparison_Approach_USD|Comparison_Approach_MXN|Final_Val_USD|Final_Val_MXN|" > header.txt
 
 # Sustituye comillas por
 sed "s/\"//g" $file > tempo
 
-
 # Elimina los 5 primeros registros
-sed "1,5d" tempo > tempo2
+sed "1,3d" tempo > tempo2
 
 # Agregar una columna vacia a la izquierda
-awk -F\, '{print " ,"$0}' tempo2 > tempo3
-
+awk -F\| '{print " |"$0}' tempo2 > tempo3
 
 # Reemplazar los #N/A por nada
 sed "s/\#N\/A//g" tempo3 > tempo4
@@ -35,19 +33,13 @@ sed "s/N\A//g" tempo5 > tempo6
 # Elimina las columnas que no pertenecen al archivo de carga
 # awk -F\, '{print $1","$3","$4","$5","sub(/,/,"",$34)","$12","$13","$16","$17","$18","$19","$20","$21","$22","$23","$24","$25","$26","$27","$28","$29","$30","$31","$32","$33","$34","$35","}' tempo6 > tempo7
 
-awk -F\, '{print $1","$3","$4","$5","$12","$13","$16","$17","$18","$19","$20","$21","$22","$23","$24","$25","$26","$27","$28","$29","$30","$31","$32","$33","$34","$35","}' tempo6 > tempo7
-
+awk -F\| '{print $1"|"$3"|"$4"|"$5"|"$11"|"$12"|"$15"|"$16"|"$17"|"$18"|"$19"|"$20"|"$21"|"$22"|"$23"|"$24"|"$25"|"$26"|"$27"|"$28"|"$29"|"$30"|"$31"|"$32"|"$33"|"$34"|"$35"|"}' tempo6 > tempo7
 
 # #Pegar los header como primera fila y enviar el output
-cat header.txt tempo7 > 2$file
+cat header.txt tempo7 > valuations_file_out.txt
 
 # # Elimina todos los archivo de tempo* que se ocupan para procesar la informacion
 rm -R tempo*
 
 # # Elimina el header.txt
 rm -R header.txt
-
-# Configurar el package
-
-
-
